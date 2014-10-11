f92f59363458179493708911a5eae5b342e2bc10
Kyle-Muir <kyle.matthew.muir@gmail.com>
2014-10-11 17:51:10 +1300

    Added TrafficManager + tests

--- Microsoft.Maps.Traffic.d.ts
+++ Microsoft.Maps.Traffic.d.ts
@@ -4,11 +4,25 @@
     NOTE: Before you can access the types found in the Microsoft.Maps.Traffic API, you must first load this module using the loadModule method.
 */
 declare module Microsoft.Maps.Traffic {
+	//NOTE: this class has been deprecated
     export class TrafficLayer {
         constructor (map: Map);
 
         getTileLayer(): TileLayer;
         hide(): void;
         show(): void;
+    }
+
+    export class TrafficManager {
+    	constructor(map: Map);
+
+    	hide(): void;
+    	hideFlow(): void;
+    	hideIncidents(): void;
+    	hideLegend(): void;
+    	show(): void;
+    	showFlow(): void;
+    	showIncidents(): void;
+    	showLegend(): void;
     }
 }
--- Microsoft.Maps.Traffic.tests.ts
+++ Microsoft.Maps.Traffic.tests.ts
@@ -8,3 +8,16 @@ function test_traffic_layer() {
 	trafficLayer.hide();
 	trafficLayer.show();
 }
+function test_traffic_manager() {
+	var map = new Microsoft.Maps.Map(document.createElement('div'));
+	var trafficManager = new Microsoft.Maps.Traffic.TrafficManager(map);
+
+	trafficManager.hide();
+	trafficManager.hideFlow();
+	trafficManager.hideIncidents();
+	trafficManager.hideLegend();
+	trafficManager.show();
+	trafficManager.showFlow();
+	trafficManager.showIncidents();
+	trafficManager.showLegend();
+}
