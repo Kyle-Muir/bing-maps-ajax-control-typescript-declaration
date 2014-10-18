e322c4ab780443d00e63e13984fcee878ad1d3e6
Kyle-Muir <kyle.matthew.muir@gmail.com>
2014-10-18 14:00:01 +1300

    Added BusinessDisambiguationSuggestion (declaration + comments) + tests

--- Microsoft.Maps.Directions.d.ts
+++ Microsoft.Maps.Directions.d.ts
@@ -29,4 +29,64 @@ declare module Microsoft.Maps.Directions {
 		**/
 		website: string;
 	}
+
+	/**
+	*	Contains a possible business result returned from geocoding a specified waypoint address or location.
+	**/
+	export class BusinessDisambiguationSuggestion {
+		/**
+		*	The address of the business result.
+		**/
+		address: string;
+
+		/**
+		*	The distance of the business result from the originally specified waypoint address or location.
+		**/
+		distance: number;
+
+		/**
+		*	The Yellow Pages ID for the business.
+		**/
+		entityId: string;
+
+		/**
+		*	The index of this suggestion within the business suggestion array.
+		**/
+		index: number;
+
+		/**
+		*	A boolean indicating whether the result location is approximate.
+		**/
+		isApproximate: boolean;
+
+		/**
+		*	The location of the business suggestion.
+		**/
+		location: Location;
+
+		/**
+		*	The name of the business.
+		**/
+		name: string;
+
+		/**
+		*	The phone number of the business suggestion.
+		**/
+		phoneNumber: string;
+
+		/**
+		*	The URL of a photo of the business suggestion.
+		**/
+		photoUrl: string;
+
+		/**
+		*	The rooftop location of the business suggestion.
+		**/
+		rooftopLocation: Location;
+
+		/**
+		*	The URL of the business result’s website.
+		**/
+		website: string;
+	}
 }
--- Microsoft.Maps.Directions.tests.ts
+++ Microsoft.Maps.Directions.tests.ts
@@ -8,3 +8,18 @@ var test_business_details = () => {
 	businessDetails.phoneNumber = '555-5555';
 	businessDetails.website = 'http://www.kyles-bagels.com';
 }
+
+var test_business_disambiguation_suggestion = () => {
+	var businessDisambiguationSuggestion = new Microsoft.Maps.Directions.BusinessDisambiguationSuggestion();
+	businessDisambiguationSuggestion.address = '1 Microsoft Way';
+	businessDisambiguationSuggestion.distance = 1234;
+	businessDisambiguationSuggestion.entityId = '123';
+	businessDisambiguationSuggestion.index = 1;
+	businessDisambiguationSuggestion.isApproximate = false;
+	var fakeLocation  = new Microsoft.Maps.Location(1, 1);
+	businessDisambiguationSuggestion.location = fakeLocation;
+	businessDisambiguationSuggestion.name = 'Microsoft';
+	businessDisambiguationSuggestion.phoneNumber = '1-800-MICROSOFT';
+	businessDisambiguationSuggestion.rooftopLocation = fakeLocation;
+	businessDisambiguationSuggestion.website = 'http://www.microsoft.com';
+}
